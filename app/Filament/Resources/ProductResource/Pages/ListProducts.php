188429e94a3f86367d028e33463ebd86ea0374df
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2024-02-16 00:05:33 +1030

    Modal closures being evauluated multiple times

--- app/Filament/Resources/ProductResource/Pages/ListProducts.php
+++ app/Filament/Resources/ProductResource/Pages/ListProducts.php
@@ -2,18 +2,57 @@
 
 namespace App\Filament\Resources\ProductResource\Pages;
 
-use App\Filament\Resources\ProductResource;
 use Filament\Actions;
+use App\Models\Product;
+use Filament\Support\Enums\MaxWidth;
+use Filament\Forms\Components\TextInput;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ListRecords;
+use App\Filament\Resources\ProductResource;
 
 class ListProducts extends ListRecords
 {
     protected static string $resource = ProductResource::class;
 
+    protected function testProp()
+    {
+        return 0;
+    }
+
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\Action::make('inviteStaffUser')
+                ->modalHidden(function () {
+                    Notification::make()
+                        ->title('Modal hidden')
+                        ->success()
+                        ->send();
+
+                    return false;
+                })
+                ->modalHeading(function () {
+                    Notification::make()
+                        ->title('Modal heading')
+                        ->warning()
+                        ->send();
+
+                    return 'Modal heading';
+                })
+                ->modalDescription(function () {
+                    Notification::make()
+                        ->title('Modal description')
+                        ->danger()
+                        ->send();
+
+                    return 'Modal description';
+                })
+                ->modalSubmitActionLabel('Modal submit')
+                ->form([
+                    TextInput::make('name')
+                        ->label('Name')
+                        ->required(),
+                ])
         ];
     }
 }
